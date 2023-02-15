e9e2b13b8562b8a9217336164dd1256cf6742e48
zds <zds@qq.com>
2023-02-15 17:23:16 +0800

    hot-fix second commit

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -9,5 +9,6 @@ public class GitTest {
         System.out.println("hello git!");
         System.out.println("hello git1!");
         System.out.println("hello hot-fix!");
+        System.out.println("hello china!");
     }
 }
