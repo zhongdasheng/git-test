e19be1e5524a407bd04cef30ebadb2a84c17102a
zds <zds@qq.com>
2023-02-15 17:18:04 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,12 @@
+package com.atguigu.git;
+
+/**
+ * @author shkstart
+ * @create 2023-02-15-17:15
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+        System.out.println("hello git1!");
+    }
+}
